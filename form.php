138611794703762f82b634a71847c430eae6cd4c
silviiiii <silvianindya11@gmail.com>
2019-08-23 08:35:25 +0700

    formulir

--- form.php
+++ form.php
@@ -1,14 +1,15 @@
 <html>
 <head><title>Login Here</title></head>
 <body>
+<tabel>
 <fieldset>
 <legend>Halaman register</legend>
-<FORM ACTION="form2.php" METHOD="POST" NAME="input">
+<FORM ACTION="form2.php" METHOD="POST" >
 <h2>Formulir pendaftaran</h2>
 No.Pendaftaran : 
-<br><input type="text" name="No.pendaftaran"><br>
+<br><input type="text" name="nop"><br>
 NISN : 
-<br><input type="text" name="NISN"><br>
+<br><input type="text" name="Nisn"><br>
 nama : 
 <br><input type="text" name="nama"><br>
 Alamat :
@@ -36,10 +37,22 @@ Nama ibu :
 <br><input type="text" name="Namaaa"><br>
 Pekerjaan ibu : 
 <br><input type="text" name="kerjaa"><br>
-penghasilan ibu : 
-<br><input type="text" name="hasil"><br>
-<input type="submit" name="Login" value="register">
-<input type="reset" name="reset" value="Reset">
+Hobby : 
+<br><input type="checkbox" name="hobi" value="berenang"> berenang
+<br><input type="checkbox" name="hobi2" value="menari"> menari
+<br><input type="checkbox" name="hobi3" value="masak"> masak
+<br><input type="checkbox" name="hobi4" value="membaca"> membaca<br>
+tinggi badan : 
+<br><input type="text" name="tng"><br>
+berat badan : 
+<br><input type="text" name="brt"><br>
+agama :
+<select name="agama">
+<option value="Islam">islam</option>
+<option value="kristen">kristen</option>
+<option value="hindu">hindu</option></select><br>
+<input type="submit" name="simpan" value="simpan">
+<input type="reset" name="hapus" value="hapus">
 </fieldset>
 </FORM>
 </body>
--- form2.php
+++ form2.php
@@ -1,35 +1,52 @@
 <?php
-if (isset($_POST['input'])) {
-$no = $_POST['No.pendaftaran'];  
-$nisn = $_POST['Nisn'];
-$nama = $_POST['nama'];
-$alamat = $_POST['alamat'];
+if (isset($_POST['simpan'])) {
+$now = $_POST['nop'];  
+$nis = $_POST['Nisn'];
+$nam = $_POST['nama'];
+$alam = $_POST['alamat'];
 $tmpt = $_POST['tempat'];
-$tgl = $_POST['tgl'];
+$tg = $_POST['tgl'];
 $asl = $_POST['asal'];
 $jk = $_POST['jenkel'];
 $jr = $_POST['jurus'];
-$ayah = $_POST['namaa'];
+$ayah = $_POST['Namaa'];
 $krj = $_POST['kerja'];
+$ibu = $_POST['Namaaa'];
+$krji = $_POST['kerjaa'];
+$tinggi = $_POST['tng'];
+$berat = $_POST['brt'];
 $agama =$_POST['agama'];
-$email =$_POST['email'];
-$pass = $_POST['password'];
 
-if ($pass == "1109"){
-    echo "No.pendaftaran : $no.<br>";
-    echo "NISN : $nisn.<br>";
-    echo "nama : $nama.<br>";
-    echo "alamat : $alamat.<br>";
+    echo "No.pendaftaran : $now.<br>";
+    echo "NISN : $nis.<br>";
+    echo "nama : $nam.<br>";
+    echo "alamat : $alam.<br>";
     echo "Tempat lahir : $tmpt.<br>";
-    echo "Tanggal lahir : $tgl.<br>";
+    echo "Tanggal lahir : $tg.<br>";
     echo "Asal sekolah : $asl.<br>";
     echo "jenis kelamin : $jk.<br>";
     echo "jurusan : $jr.<br>";
     echo "Nama ayah : $ayah.<br>";
     echo "pekerjaan ayah : $krj.<br>";
+    echo "Nama ibu : $ibu.<br>";
+    echo "pekerjaan ibu : $krji.<br>";
+    echo "hobby : $hobi.<br>";
+    if (isset($_POST['hobi'])) {
+        echo " ~ " . $_POST['hobi'] . "<br>";
+        }
+        if (isset($_POST['hobi2'])) {
+        echo " ~ " . $_POST['hobi2'] . "<br>";
+        }
+        if (isset($_POST['hobi3'])) {
+        echo " ~ " . $_POST['hobi3'] . "<br>";
+        }
+        if (isset($_POST['hobi4'])) {
+        echo " ~ " . $_POST['hobi4'] . "<br>";
+        }
+    echo "tinggi badan : $tinggi.<br>";
+    echo "berat badan : $berat.<br>";
     echo "agama : $agama.<br>";
-    echo "email : $email.<br>";
-    echo "<h2>Registrasi Berhasil.Akun Anda telah aktif</h2>";
+
 }
-}
+
 ?>
